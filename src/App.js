6d5cfd571e6a970867e76d9d2a2f50e1be48abc8
Businge931 <jimmybusinge9@gmail.com>
2023-05-06 21:31:46 +0300

    feat: setup voice command

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import "./App.css";
+import { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
 function App() {
+  useEffect(() => {
+    alanBtn({
+      key: "4e0b71b4bcc3b812827b97141c3d9b102e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          console.log(articles);
+        }
+      },
+    });
+  }, []);
+
   return <div className="App">Alan ai</div>;
 }
 
